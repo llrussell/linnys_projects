324fef45d7b630f2a8a6fe4051d439933871ac6c
Linton Russell <linton.russell@agilex.com>
2016-04-24 13:26:01 -0400

    add  a test greeting rest controller

---
+++ gradle_springboot_prac/src/main/java/com/linny/practice/pojo/Greeting.java
@@ -0,0 +1,23 @@
+package com.linny.practice.pojo;
+
+/**
+ * Created by russelll on 4/24/16.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
